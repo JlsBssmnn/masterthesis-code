02d25adacd3166bb2c20d84ec65501f80a8cab27
JlsBssmnn <julius.bossmann@mailbox.tu-dresden.de>
2023-04-12 21:20:39 +0200

    create epithelial generation config v1.1.0
    changes: increase noise and make membranes more uneven

---
+++ config/epithelial_sheet/v_1_1_0.py
@@ -0,0 +1,20 @@
+class Config:
+  angle_noise = 0.01
+  first_plane_offset = 20
+  gaussian_sigma = 2.5
+  limits = [100, 2000, 2000]
+  max_center_offset = [10, 10]
+  max_child_count = 5
+  max_dist = 60
+  max_offset_from_plane = 20
+  membrane_threshold = 2.25
+  min_child_count = 3
+  min_dist = 25
+  min_dist_local_max = 5
+  noise_distribution_max = 1.3
+  noise_intensity = 10
+  noise_resolution_factor = 10
+  plane_distance = 60
+  seed = 0
+
+config = Config()
