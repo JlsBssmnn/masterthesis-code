ec8ef8217fcfcd4ca358e4d7068e1397af09516d
JlsBssmnn <julius.bossmann@mailbox.tu-dresden.de>
2023-04-13 16:25:41 +0200

    create epithelial generation config v1.1.1
    changes: sample starting points from only 1 plane; decrease number of z-slices to 64

---
+++ config/epithelial_sheet/v_1_1_1.py
@@ -0,0 +1,20 @@
+class Config:
+  angle_noise = 0.01
+  first_plane_offset = 32
+  gaussian_sigma = 2.5
+  limits = [64, 2000, 2000]
+  max_center_offset = [10, 10]
+  max_child_count = 5
+  max_dist = 25
+  max_offset_from_plane = 32
+  membrane_threshold = 2.25
+  min_child_count = 3
+  min_dist = 15
+  min_dist_local_max = 5
+  noise_distribution_max = 1.5
+  noise_intensity = 8
+  noise_resolution_factor = 8
+  plane_distance = 100
+  seed = 0
+
+config = Config()
