3b9d5c62d1ede8f25061228c70337efc7af211b2
JlsBssmnn <julius.bossmann@mailbox.tu-dresden.de>
2023-04-28 11:41:49 +0200

    create epithelial generation v1.2
    changes: create blobs in the second channel of the image

--- config/epithelial_sheet/test.py
+++ config/epithelial_sheet/test.py
@@ -1,19 +1,43 @@
+plane_dist = 16
+z = 64
+
 class Config:
   angle_noise = 0.01
-  first_plane_offset = 10
-  gaussian_sigma = 2
-  limits = [100, 100, 20]
+  blob_config = BlobConfig()
+  first_plane_offset = z / 2
+  gaussian_sigma = 2.5
+  limits = [z, 128, 128]
   max_center_offset = [10, 10]
   max_child_count = 5
-  max_dist = 60
-  max_offset_from_plane = 10
-  membrane_threshold = 2
+  max_dist = 25
+  max_offset_from_plane = z // 2
+  membrane_threshold = 2.25
   min_child_count = 3
-  min_dist = 25
+  min_dist = 15
   min_dist_local_max = 5
-  noise_intensity = 10
-  noise_resolution_factor = 20
-  plane_distance = 50
+  noise_distribution_max = 1.5
+  noise_intensity = 8
+  noise_resolution_factor = 8
+  plane_distance = z + 1
   seed = 0
+
+class BlobConfig:
+  angle_noise=0.01
+  blob_max = 24 
+  blob_min = 5
+  first_plane_offset=plane_dist
+  max_center_offset=[20, 20]
+  max_child_count=6
+  max_dist=18
+  max_noise = 2
+  max_offset_from_plane=plane_dist
+  mean = 12
+  min_child_count=4
+  min_dist=10
+  noise_resolution = (1, 1, 1)
+  noise_shape = (16, 32, 32)
+  plane_distance=plane_dist*2
+  std = 3
+  z_scaling = 0.5
 
 config = Config()
