83d842e4c1bc039e5b7dd012b30e4d64e2d275c5
JlsBssmnn <julius.bossmann@mailbox.tu-dresden.de>
2023-06-05 23:45:14 +0200

    support multiple datasets and more dimensionalities

--- utils/neuroglancer_viewer/neuroglancer_viewer.py
+++ utils/neuroglancer_viewer/neuroglancer_viewer.py
@@ -21,33 +21,79 @@ def neuroglancer_viewer(images: list, names: list = None, scales: list = None, o
                 img = img[0]
 
             if img.ndim == 4:
-                if img.shape[-1] == 4:
+                channel_index = np.array(img.shape).argmin()
+                if channel_index == img.ndim - 1:
+                    # The channel is at the end
                     img = np.moveaxis(img, -1, 0)
                 else:
-                    assert img.shape[0] == 4
+                    assert channel_index == 0
                 if img.dtype != np.uint8:
                     img = img/np.max(img)
                     img = (img * 255).astype(np.uint8)
-                state.layers.append(
-                    name=name,
-                    layer=neuroglancer.LocalVolume(
-                        data=img,
-                        dimensions=neuroglancer.CoordinateSpace(
-                            names=['c^', 'z', 'y', 'x'],
-                            units=['', 'µm', 'µm', 'µm'],
-                            scales=(1,) + scale,
-                            coordinate_arrays=[
-                                neuroglancer.CoordinateArray(labels=['red', 'green', 'blue', 'yellow']), None, None, None,
-                            ])
-                    ),
-                    shader="""
-void main() {
-  emitRGB(vec3(toNormalized(getDataValue(0)),
-               toNormalized(getDataValue(1)),
-               toNormalized(getDataValue(2))+toNormalized(getDataValue(3))));
-}
-""",
-                )
+                if img.shape[channel_index] == 4:
+                    state.layers.append(
+                        name=name,
+                        layer=neuroglancer.LocalVolume(
+                            data=img,
+                            dimensions=neuroglancer.CoordinateSpace(
+                                names=['c^', 'z', 'y', 'x'],
+                                units=['', 'µm', 'µm', 'µm'],
+                                scales=(1,) + scale,
+                                coordinate_arrays=[
+                                    neuroglancer.CoordinateArray(labels=['red', 'green', 'blue', 'yellow']), None, None, None,
+                                ])
+                        ),
+                        shader="""
+    void main() {
+    emitRGB(vec3(toNormalized(getDataValue(0)),
+                toNormalized(getDataValue(1)),
+                toNormalized(getDataValue(2))+toNormalized(getDataValue(3))));
+    }
+    """,
+                    )
+                elif img.shape[channel_index] == 3:
+                    state.layers.append(
+                        name=name,
+                        layer=neuroglancer.LocalVolume(
+                            data=img,
+                            dimensions=neuroglancer.CoordinateSpace(
+                                names=['c^', 'z', 'y', 'x'],
+                                units=['', 'µm', 'µm', 'µm'],
+                                scales=(1,) + scale,
+                                coordinate_arrays=[
+                                    neuroglancer.CoordinateArray(labels=['red', 'green', 'blue']), None, None, None,
+                                ])
+                        ),
+                        shader="""
+    void main() {
+    emitRGB(vec3(toNormalized(getDataValue(0)),
+                toNormalized(getDataValue(1)),
+                toNormalized(getDataValue(2))));
+    }
+    """,
+                    )
+                elif img.shape[channel_index] == 2:
+                    state.layers.append(
+                        name=name,
+                        layer=neuroglancer.LocalVolume(
+                            data=img,
+                            dimensions=neuroglancer.CoordinateSpace(
+                                names=['c^', 'z', 'y', 'x'],
+                                units=['', 'µm', 'µm', 'µm'],
+                                scales=(1,) + scale,
+                                coordinate_arrays=[
+                                    neuroglancer.CoordinateArray(labels=['red', 'green']), None, None, None,
+                                ])
+                        ),
+                        shader="""
+    void main() {
+    emitRGB(vec3(toNormalized(getDataValue(0)),
+                toNormalized(getDataValue(1)), 0));
+    }
+    """,
+                    )
+                else:
+                    raise Exception(f"Image with {img.shape[channel_index]} channels not supported")
             elif img.ndim == 3:
                 state.layers.append(
                     name=name,
@@ -70,3 +116,81 @@ void main() {
         del sys.path[-1]
 
     return viewer
+
+
+def show_image(viewer, image, name='image', segmentation=False, scales=[0.5, 0.2, 0.2]):
+    with viewer.txn() as state:
+        dimensions = neuroglancer.CoordinateSpace(names=['z', 'y', 'x'],
+                                                units='nm',
+                                                scales=scales)
+        state.dimensions = dimensions
+
+        if name in state.layers:
+            del state.layers[name]
+        if image.ndim == 3:
+            if image.shape[0] == 3:
+                state.layers.append(
+                    name=name,
+                    layer=neuroglancer.LocalVolume(
+                        data=image,
+                        dimensions=neuroglancer.CoordinateSpace(
+                            names=['c^', 'x', 'y'],
+                            units=['', 'nm', 'nm'],
+                            scales=[1] + scales[-2:],
+                            coordinate_arrays=[
+                                neuroglancer.CoordinateArray(labels=['red', 'green', 'blue']), None, None, None,
+                            ])),
+                            shader="""
+    void main() {
+    emitRGB(vec3(toNormalized(getDataValue(0)),
+                toNormalized(getDataValue(1)),
+                toNormalized(getDataValue(2))));
+    }
+    """,
+                )
+            else:
+                state.layers.append(
+                    name=name,
+                    layer=neuroglancer.LocalVolume(
+                        data=image,
+                        dimensions=neuroglancer.CoordinateSpace(
+                            names=['z', 'y', 'x'],
+                            units='nm',
+                            scales=scales)
+                    )
+                )
+        elif image.ndim == 4:
+            state.layers.append(
+                name=name,
+                layer=neuroglancer.LocalVolume(
+                    data=image,
+                    dimensions=neuroglancer.CoordinateSpace(
+                        names=['c^', 'z', 'y', 'x'],
+                        units=['', 'nm', 'nm', 'nm'],
+                        scales=[1] + scales,
+                        coordinate_arrays=[
+                            neuroglancer.CoordinateArray(labels=['red', 'blue']), None, None, None,
+                        ])),
+                        shader="""
+    void main() {
+    emitRGB(vec3(toNormalized(getDataValue(0)),
+                toNormalized(getDataValue(1)), 0));
+    }
+    """,
+            )
+        elif image.ndim == 2:
+            state.layers.append(
+                name=name,
+                layer=neuroglancer.LocalVolume(
+                    data=image,
+                    dimensions=neuroglancer.CoordinateSpace(
+                        names=['x', 'y'],
+                        units='nm',
+                        scales=scales[-2:])
+                )
+            )
+        state.show_slices = False
+        state.cross_section_background_color = "#2e2e2e"
+        state.layout = "xy"
+        if segmentation:
+            state.layers[name].type = 'segmentation'
--- utils/neuroglancer_viewer/view_img_file.py
+++ utils/neuroglancer_viewer/view_img_file.py
@@ -4,34 +4,42 @@ import sys
 import pathlib
 import h5py
 import numpy as np
+import neuroglancer
+from skimage import io
 
 sys.path.append(str(pathlib.Path(__file__).parent.parent.resolve()))
 from misc import StoreDictKeyPair
-from neuroglancer_viewer import neuroglancer_viewer
+from neuroglancer_viewer import neuroglancer_viewer, show_image
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('file_path', help='Path to the file that contains the image')
-    parser.add_argument('--dataset_name', default=None, help='Name of the dataset that contains the image')
+    parser.add_argument('--dataset_name', default=None, nargs='+', help='Name of the dataset that contains the image')
     parser.add_argument('--scales', default=None, nargs=3, type=float, help='Scaling factor for the image')
     parser.add_argument('--slice', default=None, type=str, help='Specify a slice of the image that shall be shown')
     parser.add_argument('--setting', default=None, type=str, help='One of the defined settings to change the settings of neuroglancer')
     parser.add_argument('--setting_options', default=dict(), action=StoreDictKeyPair, nargs="+", metavar="KEY=VAL", help='Options that are passed to the setting')
+    parser.add_argument('--segmentation', action='store_true', help='If the image is a segmentation')
     opt = parser.parse_args()
 
+    images = []
     if opt.file_path.endswith('.h5'):
         if opt.dataset_name is None:
             raise Exception('If h5 is used, --dataset_name must be provided')
         with h5py.File(opt.file_path) as f:
-            img = np.asarray(f[opt.dataset_name])
+            for dataset_name in opt.dataset_name:
+                images.append(np.asarray(f[dataset_name]))
     elif opt.file_path.endswith('.tif') or opt.file_path.endswith('.tiff'):
-        img = io.imread(opt.file_path)
+        images = [io.imread(opt.file_path)]
     else:
         raise NotImplementedError('File extension not supported')
 
     if opt.slice is not None:
-        img = eval(f'img[{opt.slice}]') 
-    viewer = neuroglancer_viewer([img], scales=[tuple(opt.scales)], options=opt)
+        for i, image in enumerate(images):
+            images[i] = eval(f'images[i][{opt.slice}]') 
+    viewer = neuroglancer.Viewer()
+    for i, image in enumerate(images):
+        show_image(viewer, image, name=opt.dataset_name[i], segmentation=opt.segmentation)
     print(viewer)
     webbrowser.open(str(viewer), new=0, autoraise=True)
     input("Done?")
