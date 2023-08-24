4b4706dae71e77b1a5008e932692ad0b53646b83
JlsBssmnn <julius.bossmann@mailbox.tu-dresden.de>
2023-08-24 17:45:26 +0200

    add more filter; add png option; add sample config; fix small bugs

--- utils/gif_renderer/filters.py
+++ utils/gif_renderer/filters.py
@@ -1,4 +1,46 @@
+import numpy as np
+from skimage.color import label2rgb
+
 def threshold(image, threshold=127.5):
+  '''Threshold the image.'''
   image[image < threshold] = 0
   image[image >= threshold] = 255
   return image
+
+def invert(image):
+    '''Inverts a uint8 image, so white -> black and black -> white. Similarly for gray values.'''
+    return -image + 255
+
+def color2label(image):
+    '''Label a color image by assinging all voxels of the same color the same label. After creating
+    the segmentation, the segmentation is visualized as a color image.'''
+    c = image.shape[0]
+    img = image.reshape(c, -1)
+
+    new = np.zeros(image.shape[1:], dtype=int)
+    img2 = img.reshape(c, -1)
+    colors = np.unique(img2, axis=1)
+
+    label = 1
+    for i in range(colors.shape[1]):
+        color = colors[:, i]
+        mask = (img2.T == color).all(axis=1)
+        mask = mask.reshape(*image.shape[1:])
+
+        if (color == [0]*c).all():
+            continue
+        else:
+            new[mask] = label
+            label += 1
+
+    image = label2rgb(new)
+    image = (image * 255).astype(np.uint8)
+    image = np.rollaxis(image, -1, 0)
+    return image
+
+def label(image):
+    '''Visualizes a segmentation as a color image.'''
+    image = label2rgb(image)
+    image = (image * 255).astype(np.uint8)
+    image = np.rollaxis(image, -1, 0)
+    return image
--- utils/gif_renderer/gif_renderer.py
+++ utils/gif_renderer/gif_renderer.py
@@ -10,6 +10,7 @@ import h5py
 from typing import cast
 import numpy as np
 from PIL import Image
+from skimage.color import label2rgb
 import importlib
 import filters
 
@@ -75,8 +76,15 @@ def main(opt):
                     channel_selection = np.minimum(np.sum(channel_selection, axis=-1), 255)
                     image[:, :, :, 2] = channel_selection
                     image = image[:, :, :, :3]
+                elif image.shape[-1] < 3:
+                    # increase to 3 color channels
+                    missing = 3 - image.shape[-1]
+                    new_values = np.zeros(image.shape[:-1] + (missing,), dtype=np.uint8)
+                    image = np.append(image, new_values, axis=-1)
+        elif image.ndim == 3:
+            image = (label2rgb(image) * 255).astype(np.uint8)
         else:
-            assert image.ndim == 3, "Images must be either 3D or 4D"
+            raise NotImplementedError(f"Dimensionality {image.ndim} is not implemented")
 
         image = np.moveaxis(image, 1, 2) # swap x- and y-axis
         dim = image.ndim
@@ -89,12 +97,70 @@ def main(opt):
                            format='GIF', append_images=additional_frames, save_all=True,
                            duration=opt.duration, loop=opt.loop)
 
+def save_png(opt):
+    f = h5py.File(opt.input_file)
+    directories = opt.directories
+    os.makedirs(opt.output_dir, exist_ok=True)
+    images = []
+    filter_func = create_filters(opt)
+
+    for directory in directories:
+        images.extend(extract_datasets(f, directory))
+
+    def index(i: int, dim: int):
+        return tuple([(slice(None) if x != opt.slice_axis else i) for x in range(dim)])
+
+    def get_image_name(full_dataset_name, image_index):
+        if image_index < len(opt.names):
+            name = opt.names[image_index]
+            if not name.endswith('.png'):
+                name += '.png'
+            return name
+        else:
+            dataset_name = full_dataset_name[full_dataset_name.rfind('/') + 1:]
+            if dataset_name == '':
+                return 'output.png'
+            else:
+                return dataset_name + '.png'
+
+    for i, dataset in enumerate(images):
+        image = filter_func(np.asarray(dataset))
+        if image.ndim == 4:
+            assert np.argmin(image.shape) == 0, "Color channel must be in the first dimension"
+            if image.shape[0] == 1:
+                image = image[0]
+            else:
+                image = np.moveaxis(image, 0, -1)
+                if image.shape[-1] > 3:
+                    # reduce to 3 color channels like neuroglancer does
+                    channel_selection = image[:, :, :, 2:]
+                    channel_selection = np.minimum(np.sum(channel_selection, axis=-1), 255)
+                    image[:, :, :, 2] = channel_selection
+                    image = image[:, :, :, :3]
+                elif image.shape[-1] < 3:
+                    # increase to 3 color channels
+                    missing = 3 - image.shape[-1]
+                    new_values = np.zeros(image.shape[:-1] + (missing,), dtype=np.uint8)
+                    image = np.append(image, new_values, axis=-1)
+        else:
+            assert image.ndim == 3, "Images must be either 3D or 4D"
+
+        image = np.moveaxis(image, 1, 2) # swap x- and y-axis
+        dim = image.ndim
+        image = Image.fromarray(image[index(opt.png_indeces[i], dim)])
+
+        image.save(path.join(opt.output_dir, get_image_name(dataset.name, i)), format='png')
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Render gifs from an h5 file. For a sample config see the sample_config.py file.')
     parser.add_argument('config', type=str, help='Which config in the ./configs directory to use')
+    parser.add_argument('--png', action='store_true', help='If provided, PNGs will be stored at given indeces')
     opt = parser.parse_args()
 
     config = importlib.import_module(f'configs.{opt.config}').config
 
-    main(config)
+    if opt.png:
+        save_png(config)
+    else:
+        main(config)
